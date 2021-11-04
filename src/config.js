639c7b5a375a19002478d19daa55b9ed9def3e47
nazimboudeffa <boudeffa@gmail.com>
2021-11-04 19:30:18 +0100

    Initial Commit

---
+++ src/config.js
@@ -0,0 +1,26 @@
+const layersOrder = [
+    { name: 'background', number: 1 },
+    { name: 'head', number: 2}
+    //{ name: 'ball', number: 2 },
+    //{ name: 'eye color', number: 12 },
+    //{ name: 'iris', number: 3 },
+    //{ name: 'shine', number: 1 },
+    //{ name: 'shine', number: 1 },
+    //{ name: 'bottom lid', number: 3 },
+    //{ name: 'top lid', number: 3 },
+];
+  
+const format = {
+    width: 230,
+    height: 230
+};
+
+const rarity = [
+    { key: "", val: "original" },
+    { key: "_r", val: "rare" },
+    { key: "_sr", val: "super rare" },
+];
+
+const defaultEdition = 5;
+
+module.exports = { layersOrder, format, rarity, defaultEdition };
